17d8bdc1a722c27f5598d2b5985bc723b0a6df6d
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2026-02-01 11:41:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+let table = document.querySelector("#sampleTable")
+// let tr = document.querySelector("tr")
+
 function insert_Row() {
-    //Write your code here
-  
-  
+
+	let tr = document.createElement("tr")
+    let td1 = document.createElement("td")
+    let td2 = document.createElement("td")
+
+	td1.innerText = "New Cell1"
+	td2.innerText = "New Cell2"
+
+	tr.append(td1,td2)
+
+	// table.append(tr)
+
+	table.insertAdjacentElement("afterbegin", tr)
+
 }
